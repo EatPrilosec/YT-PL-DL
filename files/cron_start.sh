1b262709e3e2ff16053e32dc98a540fb8d3a27c7
EatPrilosec <ae3nerdgod@gmail.com>
2024-07-06 16:24:48 -0400

    Update cron_start.sh

--- files/cron_start.sh
+++ files/cron_start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-
 echo "Playlist="$Playlist"" >/opt/env
 echo "Playlist_name="$Playlist_name"" >>/opt/env
 echo "Playlists_Path="$Playlists_Path"" >>/opt/env
 echo "Playlist_TempPath="$Playlist_TempPath"" >>/opt/env
 echo "Playlist_Archive="$Playlist_Archive"" >>/opt/env
 
-echo "$CronSchedule /opt/get-YTMusicPlaylist.sh" >/opt/cron
+
+echo "$CronSchedule sudo --group=$PGID --user=$PUID /opt/get-YTMusicPlaylist.sh" >/opt/cron
+
 crontab /opt/cron
 touch /opt/cron.log
 cron && tail -f /opt/cron.log
