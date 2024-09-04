a0c2d648cada04bb85417201640db0a30f35a1a3
EatPrilosec <ae3nerdgod@gmail.com>
2024-09-03 23:05:50 -0400

    Update get-YTMusicPlaylist.sh

--- files/get-YTMusicPlaylist.sh
+++ files/get-YTMusicPlaylist.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-. /opt/env
+
+
+. /app/env
 echo Playlist: $Playlist 						#https://www.youtube.com/playlist?list=PLYJsyEKS11ydV1tC2wJtc3CkwczCyYNyZ
 echo Playlist_name: $Playlist_name 				#music
 echo Playlists_Path: $Playlists_Path 			#/Storage/Media/Unorganized/ytdlp/playlists
 echo Playlist_TempPath: $Playlist_TempPath 		#/Storage/.cache
 echo Playlist_Archive: $Playlist_Archive 		#/Storage/Media/Unorganized/ytdlp/mymusic-archive
 
-/opt/yt-dlp -U
-/opt/yt-dlp --version
-/opt/yt-dlp --embed-metadata --windows-filenames \
+/app/yt-dlp -U
+/app/yt-dlp --version
+/app/yt-dlp --embed-metadata --windows-filenames \
 	--parse-metadata "title:%(artist)s - %(track)s" \
 	--embed-metadata \
 	-o "$Playlist_name/%(artist|Unknown Artist)s - %(track|Unknown Song Name)s __ %(fulltitle)s.%(ext)s" -f m4a \
